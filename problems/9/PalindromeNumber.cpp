e91960ebb6bc7d5899622968dad4c71c14e25ded
Koby Curtiss <45833998+KCurtiss5@users.noreply.github.com>
2024-05-22 22:31:18 -0600

    improved reverseNum

--- problems/9/PalindromeNumber.cpp
+++ problems/9/PalindromeNumber.cpp
@@ -35,7 +35,7 @@ public:
 private:
     long reverseNum(int x){
         long rev = 0;
-        while (x > 0) {
+        while (x != 0) {
             rev = rev * 10 + x % 10;
             x /= 10;
         }
