53c2dc11d523daa3ae7bdb5d1f2e59ee6953991d
Koby Curtiss <45833998+KCurtiss5@users.noreply.github.com>
2024-06-25 13:54:51 -0600

    finished #2

--- problems/2/addTwoNumbers.cpp
+++ problems/2/addTwoNumbers.cpp
@@ -46,3 +46,45 @@ list of its own. However, this fails test cases (even with a long long sum). Thi
 list in this way is storing an incredibly long number. So I have to re-design the algorithm, likely to stream read the numbers
 and carry the ten.
 */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *head = new ListNode();
+        ListNode *cur = head;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int sum = (l1 != nullptr ? l1->val : 0) +
+                      (l2 != nullptr ? l2->val : 0) +
+                      carry;
+
+            carry = sum / 10;
+            sum %= 10;
+
+            cur->next = new ListNode(sum);
+            cur = cur->next;
+
+            l1 = (l1 != nullptr) ? l1->next : nullptr;
+            l2 = (l2 != nullptr) ? l2->next : nullptr;
+        }
+
+        return head->next;
+    }
+};
+
+/*
+Implements algorithm from above where we read digits in a stream-like fashion and creates a linkedlist as it goes.
+Runtime - 19ms Beats 71.25%
+Memory - 75.98 MB Beats 86.71%
+*/
