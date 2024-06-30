4a8714e39419a5689e4a9fd2b52266c1fc623dbd
Koby Curtiss <45833998+KCurtiss5@users.noreply.github.com>
2024-06-29 19:55:19 -0600

    DRY code

--- problems/13/RomanToInteger.cpp
+++ problems/13/RomanToInteger.cpp
@@ -76,3 +76,34 @@ public:
 This now handles invalid roman numerals like IC. However, we can see repeated code. its WET - WET (Write Everything Twice) 
 and DRY (Don't Repeat Yourself). So lets be DRY.
 */
+
+#include <stdexcept>
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        int sum = 0;
+        map<char, int> roman { 
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+        for (int i = 0; i < s.length()-1; i++){
+            int j = roman[s[i]];
+            int k = roman[s[i+1]];
+            if (j < k){
+                if ((j == 1 && (k == 5 || k == 10)) || 
+                    (j == 10 && (k == 50 || k == 100)) || 
+                    (j == 100 && (k == 500 || k == 1000))) {
+                    sum -= j;
+                }
+                else {
+                    throw invalid_argument("invalid roman numeral");
+                }
+            }
+            else {
+                sum += roman[s[i]];
+            }
+        }
+        sum += roman[s[s.length()-1]];
+        return sum;
+    }
+};
