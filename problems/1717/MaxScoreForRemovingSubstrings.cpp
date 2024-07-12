b5d691762a6cab9d622b8480026187a38d62a93a
Koby Curtiss <45833998+KCurtiss5@users.noreply.github.com>
2024-07-12 10:57:16 -0600

    upload better solution

--- problems/1717/MaxScoreForRemovingSubstrings.cpp
+++ problems/1717/MaxScoreForRemovingSubstrings.cpp
@@ -43,4 +43,47 @@ you're done and it returns the score.
 This is a very intuitive solution, but slow. 
 
 So we need a better solution that is less reliant on the string.find() function.
+
+The find algorithm is a linear algorithm that finds the first occurance of the substring. If we can keep track of where to start
+we can avoid having to search over the first part of the string linearly every time.
+*/
+
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        if (x > y) {
+            return calculateMaxGain(s, "ab", x, "ba", y);
+        } else {
+            return calculateMaxGain(s, "ba", y, "ab", x);
+        }
+    }
+
+private:
+    int calculateMaxGain(string s, const string& firstPair, int firstValue, const string& secondPair, int secondValue) {
+        int score = 0;
+        score += removePairs(s, firstPair, firstValue);
+        score += removePairs(s, secondPair, secondValue);
+        return score;
+    }
+
+    int removePairs(string& s, const string& pair, int value) {
+        int score = 0;
+        size_t start = 0;
+        size_t find;
+        while ((find = s.find(pair, start)) != string::npos) {
+            s.erase(find, 2);
+            score += value;
+            start = find;
+            if (start > 0) start-=1;
+        }
+        return score;
+    }
+};
+
+/*
+This passes. It utilizes a few optimizations that the first doesn't. Primarily using start in find to reduce the wasted time
+searching the beginning of the string. It's still slow, but I spent too long on this problem lol.
+
+Runtime - 707ms Beats 5.29%
+Memory - 20.72MB Beats 71.95%
 */
