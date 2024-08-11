4feb9b6668a6623f6c273c744eb563768541d0d5
Koby Curtiss <45833998+KCurtiss5@users.noreply.github.com>
2024-08-11 12:54:58 -0600

    upload second length of last word solution

--- problems/58/LengthofLastWord.cpp
+++ problems/58/LengthofLastWord.cpp
@@ -20,3 +20,25 @@ Memory - 13.57MB Beats 5.10%
 You can see that this algorithm is O(N). Regex will always start parsing from the start. So its not very efficient nor is it readable.
 */
 
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length = 0;
+        for (int i = s.size()-1; i >= 0; i--){
+            if (s[i] != ' '){
+                length++;
+            }
+            else if (s[i] == ' ' && length != 0) break;
+        }
+        return length;
+    }
+};
+
+/*
+This solution is a bit easier to read. We start reading the string backwards. If we see a space, we do nothing. The moment we start not
+seeing spaces is when we start incrementing the length. And the moment we see a space after we've seen characters, we break.
+This is also O(N) as worst case, but much much better average case.
+
+Runtime - 0ms Beats 100.00% O(N)
+Memory - 7.82MB Beats 46.74%
+*/
