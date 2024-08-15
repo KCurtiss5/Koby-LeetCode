74d39930fbf145d82400db382d515e1813b62dd8
Koby Curtiss <45833998+KCurtiss5@users.noreply.github.com>
2024-08-15 14:17:37 -0600

    linear solution to climbing stairs

--- problems/70/ClimbingStairs.cpp
+++ problems/70/ClimbingStairs.cpp
@@ -15,3 +15,26 @@ gone to. Either, you do one of the solutions for climbStairs(n-1) and one step o
 We would have two options to fix this. One is memoization. The other is changing the algorithm to not be recursive. I opted for the 
 latter.
 */
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int oneStepBefore = 2; // Ways to climb n-1 steps
+        int twoStepsBefore = 1; // Ways to climb n-2 steps
+
+        for (int i = 3; i <= n; i++) {
+            int current = oneStepBefore + twoStepsBefore;
+            twoStepsBefore = oneStepBefore;
+            oneStepBefore = current;
+        }
+        return oneStepBefore;
+    }
+};
+
+/*
+Runtime - 0ms Beats 100%
+Memory - 7.27MB Beats 78.26%
+*/
