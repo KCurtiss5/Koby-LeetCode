977a808f6481bd92c3a1bc8cb2c54e4f3be57b64
Koby Curtiss <45833998+KCurtiss5@users.noreply.github.com>
2024-08-31 20:18:35 -0600

    upload better solution for 2022

--- problems/2022/1dArrayTo2dArray.cpp
+++ problems/2022/1dArrayTo2dArray.cpp
@@ -20,3 +20,26 @@ But its slow compared to other implementations. Why is that? Let's see if we can
 Runtime - 98ms Beats 8.92%
 Memory - 95.26MB Beats 13.33%
 */
+
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if (m*n != original.size()) return {};
+        vector<vector<int>> vects(m, vector<int>(n));
+        for (int i = 0; i < original.size(); ++i){
+            vects[i / n][i % n] = original[i];
+        }
+        return vects;
+    }
+};
+
+/*
+I made a couple of changes. First, I can just return {} because c++ can do the implicit conversion.
+Next, we can completely forgo the for loop by using the fill constructor for vector. We can say how many vectors we need, and how
+big those vectors are.
+Next, we have to change up my for loop to add elements, but we can use the trick vects[i/n][i%n] to put everything in its proper place.
+The last change is quite pedantic, but pre-increment is faster than post-increment.
+This is much better!
+Runtime - 80 ms Beats 65.77%
+Memory - 88 MB Beats 83.87%
+*/
